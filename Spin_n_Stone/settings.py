733326a97cc6d7336958e1a862f4bdc2a404bdbf
shamblini <78273476+shamblini@users.noreply.github.com>
2022-11-03 13:03:45 -0500

    Changed to python v3.10

--- Spin_n_Stone/settings.py
+++ Spin_n_Stone/settings.py
@@ -89,14 +89,25 @@ WSGI_APPLICATION = 'Spin_n_Stone.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': dj_database_url.config(        
-        # Feel free to alter this value to suit your needs.        
-        default='postgresql://postgres:postgres@localhost:5432/Spin_n_Stone',        
-        conn_max_age=600    
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(        
+#         # Feel free to alter this value to suit your needs.        
+#         default='postgresql://postgres:postgres@localhost:5432/Spin_n_Stone',        
+#         conn_max_age=600    
+#     )
 
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'csce315_911_13',
+        'USER': 'csce315_911_soqui',
+        'PASSWORD': '730001998',
+        'HOST': 'csce-315-db.engr.tamu.edu',
+        'PORT': '5432',
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
