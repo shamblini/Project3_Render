5a3dcc048f0418710c58cd16ce15727389e364fa
shamblini <78273476+shamblini@users.noreply.github.com>
2022-11-06 17:53:21 -0600

    Added primitive connection demo

--- login/views.py
+++ login/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from login.models import Inventory
 
 # Create your views here.
 def loginScreen(request):
-    return render(request, 'html/loginScreen.html', {})
+    inventory_objs = Inventory.objects.all()
+    context = {
+        "inventory": inventory_objs
+    }
+    return render(request, 'html/loginScreen.html', context)
