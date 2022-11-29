3139a77c46d5015e467c8d87c62e173d6fb90af0
shamblini <78273476+shamblini@users.noreply.github.com>
2022-11-29 13:47:53 -0600

    attempt2

--- Spin_n_Stone/settings.py
+++ Spin_n_Stone/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_SAVE_EVERY_REQUEST = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
